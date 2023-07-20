ea78b0d96487e1de21f389075f3b421eb5af1900
the-akleem91 <97107720+the-akleem91@users.noreply.github.com>
2023-07-20 16:01:23 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -11,58 +11,59 @@ class Solution {
   public:
 
   
-  void bfs(int i , int j , int x , int y , vector<vector<int>>&grid , 
-  vector<vector<int>>&vis , vector<pair<int,int>>&res)
-  {
-      
-      int m = grid.size();
-      int n = grid[0].size();
-      
-      vis[i][j] = 1;
-      res.push_back({i-x , j-y});
-      
-      vector<int> nrow = {-1 , 0 , 1  , 0};
-      vector<int> ncol = { 0,   -1 , 0 , 1 };
-      
-      for(int k = 0 ; k < 4 ; k++)
-              {
-               int row = i + nrow[k];
-               int col = j + ncol[k];
-                     
-                if(row >= 0 && row < m && col >= 0 && col < n 
-                && grid[row][col] == 1 && vis[row][col]==0)
-                {
-          
-                    vis[row][col] = 1;
-                    bfs(row , col , x , y, grid , vis , res);
-                }
-              }
-      }
-  
-    int countDistinctIslands(vector<vector<int>>& grid) {
-        // code here
-        int m = grid.size();
-        int n = grid[0].size();
-        
-       
-        vector<vector<int>> vis (m , vector<int> (n , 0));
-        set<vector<pair<int,int>>> s;
-          
-        for(int i = 0 ; i < m ; i++)
-        {
-            for(int j = 0 ; j < n ; j++)
-            {
-                if(vis[i][j] == 0 && grid[i][j]==1) 
-                {         
-                   vector<pair<int,int>> res;
-                   bfs( i , j , i , j , grid , vis , res);
-                   s.insert(res);
-                }
+  void bfs(int x, int y, vector<vector<int>>& grid,
+         vector<vector<int>>& vis, vector<pair<int, int>>& res) {
+
+    int m = grid.size();
+    int n = grid[0].size();
+
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    vis[x][y] = 1;
+
+    vector<int> nrow = {-1, 0, 1, 0};
+    vector<int> ncol = {0, -1, 0, 1};
+
+    while (!q.empty()) {
+        int i = q.front().first;
+        int j = q.front().second;
+        q.pop();
+
+        res.push_back({i - x, j - y}); // Using absolute coordinates
+
+        for (int k = 0; k < 4; k++) {
+            int row = i + nrow[k];
+            int col = j + ncol[k];
+
+            if (row >= 0 && row < m && col >= 0 && col < n &&
+                grid[row][col] == 1 && vis[row][col] == 0) {
+                vis[row][col] = 1;
+                q.push({row, col});
             }
         }
-        
-        return s.size();
     }
+}
+
+
+  int countDistinctIslands(vector<vector<int>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+
+    vector<vector<int>> vis(m, vector<int>(n, 0));
+    set<vector<pair<int, int>>> s;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (vis[i][j] == 0 && grid[i][j] == 1) {
+                vector<pair<int, int>> res;
+                bfs(i, j, grid, vis, res);
+                s.insert(res);
+            }
+        }
+    }
+
+    return s.size();
+}
 };
 
 
